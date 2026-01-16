934197a0928c46d56cbf93ea98251c1337b39924
Bhumi Singh <bhumi.singh@iphtechnologies.com>
2026-01-16 17:59:06 +0530

    added delete folder option

--- QuickNoteApp/Home/View /FolderCollectionViewCell.swift
+++ QuickNoteApp/Home/View /FolderCollectionViewCell.swift
@@ -5,23 +5,39 @@
 //  Created by iPHTech 22 on 02/01/26.
 //
 import UIKit
+
 class FolderCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
 
+   
+    var onDeleteRequest: (() -> Void)?
+
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        
+        titleLabel.textAlignment = .center
         iconImageView.contentMode = .scaleAspectFit
         iconImageView.clipsToBounds = true
 
-   
+        // 1. Enable interaction so it can be clicked
+        iconImageView.isUserInteractionEnabled = true
+        
+        // 2. Add Tap Gesture
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleIconTap))
+        iconImageView.addGestureRecognizer(tap)
+
         iconImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             iconImageView.widthAnchor.constraint(equalToConstant: 40),
             iconImageView.heightAnchor.constraint(equalToConstant: 40)
         ])
+    }
+
+    @objc private func handleIconTap() {
+        // 3. Trigger the delete request
+        onDeleteRequest?()
     }
 
     func configure(with model: FolderModel) {
